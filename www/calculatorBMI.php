1f2d12ce56b8d5c40a56df816fdf13487fdafef2
WinnyMedina <winnymedinaa@gmail.com>
2016-05-19 22:06:35 -0600

    Update calculatorBMI.php

--- www/calculatorBMI.php
+++ www/calculatorBMI.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta name="viewport" content="width=device-width, user-scalable, initial-scale=1.0", maximum-scale=1.0, minimum-scale=1.0>
-	<meta charset="UTF-8">
-	<title>Extreme Health Help</title>
-	<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Measure Mass</title>
 	<link rel=-shortcut icon- href=/imgs/ima.ico />
 </head>
     
@@ -14,67 +13,213 @@
 	
    
    
-   <!--Content-->
-<div id="contentIndex">
-
-	<div class="headerIndex">
-		<h3>Extreme healht help</h3>	
-	</div>
-	
-    <div class="contentIMC" >
-		<div class="main">
-		<div class="calculatorIMC">
-		<!--Logo-->
-    	<div id="logo"></div>
-    <!--Fim Logo-->
-    
-    <!--Calculadora-->
-        <div id="calculadora">
-    	<!--Formulario-->
-        <form name="calc" method="get" enctype="multipart/form-data" action="resultBMI.php" class="form">
-        	
-            <fieldset class="f">
-            	
-                <label for="">Género</label>
-                <br/>
-                <form action="">
-				
-                  <input class="genero"  type="radio" name="gender" value="male"><img src="img/masculine.png"  width="50px" height="50px">
-                  <input class="genero"  type="radio" name="gender" value="female"> <img src="img/femenine.png"  width="50px" height="50px">
-                </form>
-                <br/>
-                <label for="">Peso</label>
-                <br/>
-                <input  type="text" name="peso" class="in" /><label for="">kg</label>
-                <br />
-                <label for="">Estatura</label>
-                <br/>
-                <input type="text" name="altura" class="in"/><label for="">cm</label><br/>
-                
-<!--                <button class="buttomCalculo" "submit" name="envia" >Calcular!</button>-->
-            
-            </fieldset></form>
-            
-             <button class="w3-btn w3-red w3-round-large" >Calcular!</button>
-            
+<!--Content-->
+	<div class="w3-container w3-light-grey">
+	    <div class="w3-container w3-red" id="header">
+		    <h3>Measure Mass</h3>	
 	    </div>
-        
-        <div ><img src="img/salud.gif"  width="350px" height="200px"></div>
-			
-        <!--Fim Formulario-->
-        </div>
-        </div>
-    </div>    
-    <!--Fim Calculadora-->
+		
+	<div class="w3-container w3-section" id="header-content">
+   		<div class="w3-container w3-red">
+  			<h2>Complete los datos solicitados</h2>
+		</div>
+		<div class="w3-container w3-section w3-border">
+<form class="w3-container" action="form.asp">
+<p><label class="w3-label w3-text-grey"><b>Genero</b></label></p>
+<input class="w3-radio" type="radio" name="gender">
+<label class="w3-validate"><img src="img/masculine.png"  width="50px" height="50px"></label>
 
-        <div id="footer">
+<input class="w3-radio" type="radio" name="gender">
+<label class="w3-validate"><img src="img/femenine.png"  width="50px" height="50px"></label>
+
+<p>      
+<label class="w3-label w3-text-grey "><b>Peso</b></label>
+<input class="w3-input w3-border w3-sand" name="first" type="text">
+<label class="w3-label w3-text-grey "><b>Kg</b></label>
+</p>
+<p>      
+<label class="w3-label w3-text-grey"><b>Estatura</b></label>
+<input class="w3-input w3-border w3-sand" name="last" type="text">
+<label class="w3-label w3-text-grey"><b>Cm</b></label>
+</p>
+
+	
+		<br>
+		<input type="button" class=" w3-xlarge w3-buttom w3-green w3-hover-blue w3-round-xlarge" onclick="location.href='resultBMI.php'" value="Calcular" name="enter"></input> 
+		<br><br>
+			</div>
+		 <div><img src="img/salud.gif"  width="350px" height="200px"></div>
+	</form>
+
+</div>
+       <div class="w3-container w3-red" id="footer">
 		    <h2>&#169;CopyRight</h2>
 		
-		    <div class="images">
-		        <a href="https://www.instagram.com"><img src="img/instagram.png"  width="40px" height="40px"></a>
-		        <a href="https://www.facebook.com"><img src="img/facebook.png" width="30px" height="30px"></a>	
-		        <a href="https://accounts.google.com/ServiceLogin?sacu=1&continue=https%3A%2F%2Fphotos.google.com%2Flogin&followup=https%3A%2F%2Fphotos.google.com%2Flogin&osid=1#identifier"><img src="img/google+.png"  width="30px" height="30px"></a>	
-		        <a href="https://twitter.com"><img src="img/twitter.png"  width="30px" height="30px"></a>	
+		    <div class="w3-contanier">
+            <ul>
+                <li class="w3-padding-16"><a href="https://www.instagram.com"><img class="w3-center w3-circle" style="width:40px" src="img/instagram.png"></a></li>
+                <li class="w3-padding-20"><a href="https://www.facebook.com"><img class="w3-center w3-circle" style="width:40px" src="img/facebook.png" ></a></li>
+                <li class="w3-padding-16"><a href="https://accounts.google.com/ServiceLogin?sacu=1&continue=https%3A%2F%2Fphotos.google.com%2Flogin&followup=https%3A%2F%2Fphotos.google.com%2Flogin&osid=1#identifier"><img class="w3-center w3-circle" style="width:40px" src="img/google+.png"></a></li>
+                <li class="w3-padding-16"> <a href="https://twitter.com"><img class="w3-center w3-circle" style="width:40px" src="img/twitter.png" ></a></li>
+            </ul>
+		      
+		        	
+		        	
+		       	
+		    </div>
+        </div>
+	
+	</div>
+</div>
+<!--Fim Content-->   
+</body>
+    
+</html>
+	<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Measure Mass</title>
+	<link rel=-shortcut icon- href=/imgs/ima.ico />
+</head>
+    
+<body>
+
+	
+   
+   
+<!--Content-->
+	<div class="w3-container w3-light-grey">
+	    <div class="w3-container w3-red" id="header">
+		    <h3>Measure Mass</h3>	
+	    </div>
+		
+	<div class="w3-container w3-section" id="header-content">
+   		<div class="w3-container w3-red">
+  			<h2>Complete los datos solicitados</h2>
+		</div>
+		<div class="w3-container w3-section w3-border">
+<form class="w3-container" action="form.asp">
+<p><label class="w3-label w3-text-grey"><b>Genero</b></label></p>
+<input class="w3-radio" type="radio" name="gender">
+<label class="w3-validate"><img src="img/masculine.png"  width="50px" height="50px"></label>
+
+<input class="w3-radio" type="radio" name="gender">
+<label class="w3-validate"><img src="img/femenine.png"  width="50px" height="50px"></label>
+
+<p>      
+<label class="w3-label w3-text-grey "><b>Peso</b></label>
+<input class="w3-input w3-border w3-sand" name="first" type="text">
+<label class="w3-label w3-text-grey "><b>Kg</b></label>
+</p>
+<p>      
+<label class="w3-label w3-text-grey"><b>Estatura</b></label>
+<input class="w3-input w3-border w3-sand" name="last" type="text">
+<label class="w3-label w3-text-grey"><b>Cm</b></label>
+</p>
+
+	
+		<br>
+		<input type="button" class=" w3-xlarge w3-buttom w3-green w3-hover-blue w3-round-xlarge" onclick="location.href='resultBMI.php'" value="Calcular" name="enter"></input> 
+		<br><br>
+			</div>
+		 <div><img src="img/salud.gif"  width="350px" height="200px"></div>
+	</form>
+
+</div>
+       <div class="w3-container w3-red" id="footer">
+		    <h2>&#169;CopyRight</h2>
+		
+		    <div class="w3-contanier">
+            <ul>
+                <li class="w3-padding-16"><a href="https://www.instagram.com"><img class="w3-center w3-circle" style="width:40px" src="img/instagram.png"></a></li>
+                <li class="w3-padding-20"><a href="https://www.facebook.com"><img class="w3-center w3-circle" style="width:40px" src="img/facebook.png" ></a></li>
+                <li class="w3-padding-16"><a href="https://accounts.google.com/ServiceLogin?sacu=1&continue=https%3A%2F%2Fphotos.google.com%2Flogin&followup=https%3A%2F%2Fphotos.google.com%2Flogin&osid=1#identifier"><img class="w3-center w3-circle" style="width:40px" src="img/google+.png"></a></li>
+                <li class="w3-padding-16"> <a href="https://twitter.com"><img class="w3-center w3-circle" style="width:40px" src="img/twitter.png" ></a></li>
+            </ul>
+		      
+		        	
+		        	
+		       	
+		    </div>
+        </div>
+	
+	</div>
+</div>
+<!--Fim Content-->   
+</body>
+    
+</html>
+               <!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Measure Mass</title>
+	<link rel=-shortcut icon- href=/imgs/ima.ico />
+</head>
+    
+<body>
+
+	
+   
+   
+<!--Content-->
+	<div class="w3-container w3-light-grey">
+	    <div class="w3-container w3-red" id="header">
+		    <h3>Measure Mass</h3>	
+	    </div>
+		
+	<div class="w3-container w3-section" id="header-content">
+   		<div class="w3-container w3-red">
+  			<h2>Complete los datos solicitados</h2>
+		</div>
+		<div class="w3-container w3-section w3-border">
+<form class="w3-container" action="form.asp">
+<p><label class="w3-label w3-text-grey"><b>Genero</b></label></p>
+<input class="w3-radio" type="radio" name="gender">
+<label class="w3-validate"><img src="img/masculine.png"  width="50px" height="50px"></label>
+
+<input class="w3-radio" type="radio" name="gender">
+<label class="w3-validate"><img src="img/femenine.png"  width="50px" height="50px"></label>
+
+<p>      
+<label class="w3-label w3-text-grey "><b>Peso</b></label>
+<input class="w3-input w3-border w3-sand" name="first" type="text">
+<label class="w3-label w3-text-grey "><b>Kg</b></label>
+</p>
+<p>      
+<label class="w3-label w3-text-grey"><b>Estatura</b></label>
+<input class="w3-input w3-border w3-sand" name="last" type="text">
+<label class="w3-label w3-text-grey"><b>Cm</b></label>
+</p>
+
+	
+		<br>
+		<input type="button" class=" w3-xlarge w3-buttom w3-green w3-hover-blue w3-round-xlarge" onclick="location.href='resultBMI.php'" value="Calcular" name="enter"></input> 
+		<br><br>
+			</div>
+		 <div><img src="img/salud.gif"  width="350px" height="200px"></div>
+	</form>
+
+</div>
+       <div class="w3-container w3-red" id="footer">
+		    <h2>&#169;CopyRight</h2>
+		
+		    <div class="w3-contanier">
+            <ul>
+                <li class="w3-padding-16"><a href="https://www.instagram.com"><img class="w3-center w3-circle" style="width:40px" src="img/instagram.png"></a></li>
+                <li class="w3-padding-20"><a href="https://www.facebook.com"><img class="w3-center w3-circle" style="width:40px" src="img/facebook.png" ></a></li>
+                <li class="w3-padding-16"><a href="https://accounts.google.com/ServiceLogin?sacu=1&continue=https%3A%2F%2Fphotos.google.com%2Flogin&followup=https%3A%2F%2Fphotos.google.com%2Flogin&osid=1#identifier"><img class="w3-center w3-circle" style="width:40px" src="img/google+.png"></a></li>
+                <li class="w3-padding-16"> <a href="https://twitter.com"><img class="w3-center w3-circle" style="width:40px" src="img/twitter.png" ></a></li>
+            </ul>
+		      
+		        	
+		        	
+		       	
 		    </div>
         </div>
 	
